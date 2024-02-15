0fd409673d8413fb4ce96b7dbdc8c552aa36d4cb
FrMnJ <manuelserratossec38@gmail.com>
2024-02-15 09:49:34 -0600

    Add resource routes Post and Comunidad

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\ComunidadController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/posts',PostController::class);
+Route::resource("/comunidades",ComunidadController::class);
