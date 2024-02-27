cf7fca85f5bf46ecf07b184fb678e520119f02ef
FrMnJ <manuelserratossec38@gmail.com>
2024-02-26 21:44:03 -0600

    Add jetstream

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,13 @@ Route::get('/', function () {
 
 Route::resource('/posts',PostController::class);
 Route::resource("/comunidades",ComunidadController::class)->parameters(['comunidades' => 'comunidad',]);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
