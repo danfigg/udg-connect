11151aa6362375b9a9b3c01016b12e22451bdaad
FrMnJ <alan.olvera5111@alumnos.udg.mx>
2024-02-27 16:34:34 -0600

    Add basic post component

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,15 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route("landing");
 });
 
 Route::resource('/posts',PostController::class);
 Route::resource("/comunidades",ComunidadController::class)->parameters(['comunidades' => 'comunidad',]);
+
+Route::get('/land',function(){
+    return view('landing');
+})->name("landing");
 
 Route::middleware([
     'auth:sanctum',
