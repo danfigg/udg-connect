cff90d8315d09b806372f8867f2dabd4a9c2ac63
FrMnJ <manuelserratossec38@gmail.com>
2024-03-18 15:25:38 -0600

    Change style of comunidades index
    Add style to crear comunidad link
    Add create icon.
    Create a new component to display comunidades info.

---
+++ resources/views/components/comunidad-info.blade.php
@@ -0,0 +1,45 @@
+@php
+    if(strlen($comunidad->descripcion) > 20){
+        $comunidad->descripcion = substr($comunidad->descripcion,0,20).'...';
+    }
+
+    if($comunidad->estado_comunidad == 'activo'){
+        $status_color = 'bg-lime-800';
+        $comunidad->estado_comunidad = "Activa";
+    }else{
+        $status_color = 'bg-red-500';
+        $comunidad->estado_comunidad = "Inactiva";
+    }
+
+    $colors = ['bg-red-500', 'bg-blue-500', 'bg-green-500', 'bg-yellow-500', 'bg-indigo-500', 'bg-purple-500', 'bg-pink-500'];
+    $banner_color = $colors[array_rand($colors)]; 
+@endphp
+
+<li class="dark:text-white px-10 py-5 border border-gray-400 mt-4 flex flex-basis">
+    <div class="flex-grow">
+        <a class="text-lg" href="{{ route('comunidades.show', $comunidad->id) }}">{{ $comunidad->nombre }}</a><br>
+        <div class="flex gap-2 items-center">
+            <div class="w-3 h-3 {{$status_color}} rounded-full"> </div>
+            <p>{{$comunidad->estado_comunidad}}</p>
+        </div>
+        <p class="text-sm dark:text-gray-300">Creador: {{$comunidad->user->name}}</p>
+        <p class="text-sm dark:text-gray-300">{{ $comunidad->descripcion }}</p>
+        <p class="text-sm dark:text-gray-300">Participantes: {{ $comunidad->num_participantes }}</p>
+        @if($comunidad->user_id == Auth::id())
+            <div class="flex gap-3">
+                <a href="{{ route('comunidades.edit', $comunidad->id) }}"><img src="/edit.svg" alt="Modificar comunidad"></a>
+                <form method="POST" action="{{ route('comunidades.destroy', $comunidad->id) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button><img src="/delete.svg" alt="Borrar comunidad"></button>
+                </form>
+            </div>
+        @endif
+    </div>
+    @if($comunidad->banner)
+        <img class="w-7/12" src="{{ asset('storage/'.$comunidad->banner) }}" alt="Banner de la comunidad {{$comunidad->nombre}}">
+    @else
+        <div class="{{$banner_color}} w-7/12">
+        </div>
+    @endif
+</li>
--- resources/views/comunidades/index.blade.php
+++ resources/views/comunidades/index.blade.php
@@ -1,23 +1,28 @@
 <x-app-layout>
+    <!-- Header -->
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Comunidades') }}
         </h2>
     </x-slot>
-    <a href="{{ route('comunidades.create') }}">Crear comunidad</a>
-    <ul>
-        @forelse ($comunidades as $comunidad)
-            <li>
-                <a href="{{ route('comunidades.show', $comunidad->id) }}">{{ $comunidad->nombre }}</a><br>
-                <a href="{{ route('comunidades.edit', $comunidad->id) }}"><img src="/edit.svg" alt="Modificar comunidad"></a>
-                <form method="POST" action="{{ route('comunidades.destroy', $comunidad->id) }}">
-                    @csrf
-                    @method('DELETE')
-                    <button><img src="/delete.svg" alt="Borrar comunidad"></button>
-                </form>
-            </li>
-        @empty
-            <p>No data comunidades</p>
-        @endforelse
-    </ul>
+    <!-- Page Content -->
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
+                <section class="dark:text-white flex justify-center py-4 mt-3">
+                    <a class="flex gap-2" href="{{ route('comunidades.create') }}">
+                        <svg class="dark:fill-white" xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 -960 960 960"><path d="M440-280h80v-160h160v-80H520v-160h-80v160H280v80h160v160Zm40 200q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-80q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Zm0-320Z"/></svg>
+                        Crear comunidad
+                    </a>
+                </section>
+                <ul class="py-5 px-5">
+                    @forelse ($comunidades as $comunidad)
+                        <x-comunidad-info :comunidad='$comunidad' />
+                    @empty
+                        <p>Oops, no hay comunidades a las que unirte.</p>
+                    @endforelse
+                </ul>    
+            </div>
+        </div>
+    </div>
 </x-app-layout>
