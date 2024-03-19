1b1195d70f686ee34c287b763826627453c0ebb5
FrMnJ <manuelserratossec38@gmail.com>
2024-03-18 21:13:21 -0600

    Add modal create post
    Using livewire modal CreatePost.
    Add CreatePost.php and create-post.blade.php.
    Add button and styles to open modal.

---
+++ resources/views/livewire/create-post.blade.php
@@ -0,0 +1,39 @@
+<div class="py-8 px-8 bg-gray-100 dark:bg-gray-900">
+<form action="{{route('posts.store')}}" method="post" class="dark:text-white">
+        @csrf 
+        <div class="flex flex-col">
+            <label for="titulo">Titulo:</label>
+            <input type="text" name="titulo" id="titulo" value="{{old('titulo')}}">
+            @error('titulo')
+              <small class="error">{{ $message }}</small>
+            @enderror
+        </div>
+        <div class="flex flex-col">
+            <label for="contenido">Contenido:</label>
+            <textarea name="contenido" id="contenido" cols="30" rows="5" >{{old('contenido')}}</textarea>
+            @error('contenido')
+              <small class="error">{{ $message }}</small>
+            @enderror
+        </div>
+        <div class="flex flex-col">
+            <label for="semestre">Semestre:</label>
+            <select class="text-black" name="semestre" id="semestre">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+              <option value="all">all</option>
+            </select>
+            @error('semestre')
+              <small class="error">{{ $message }}</small>
+            @enderror
+        </div>
+        <x-button class="mt-5">Crear Post</x-button>
+    </form>
+</div>
