501818a608d277bf0e1621b3b3774269d2d99f54
FrMnJ <manuelserratossec38@gmail.com>
2024-03-18 20:28:04 -0600

    Modify comunidades.show layout

--- resources/views/comunidades/show.blade.php
+++ resources/views/comunidades/show.blade.php
@@ -1,13 +1,15 @@
-@extends('layouts.app')
-
-@section('title', 'Show')
-
-@section('content')
-    <a href="{{ route('comunidades.index') }}">Back to index</a><br><br>
-    <h1>Comunidad</h1>
-    <p>Nombre: {{ $comunidad->nombre }}</p>
-    <p>Descripción: {{ $comunidad->descripcion }}</p>
-    <p>Reglas: {{ $comunidad->reglas }}</p>
-    <p>Numero de participantes: {{ $comunidad->num_participantes }}</p>
-    <p>Estado: {{ $comunidad->estado_comunidad }}</p>
-@endsection
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __("$comunidad->nombre") }}
+        </h2>
+    </x-slot>
+    <div class="dark:text-white">
+        <h1>Comunidad</h1>
+        <p>Nombre: {{ $comunidad->nombre }}</p>
+        <p>Descripción: {{ $comunidad->descripcion }}</p>
+        <p>Reglas: {{ $comunidad->reglas }}</p>
+        <p>Numero de participantes: {{ $comunidad->num_participantes }}</p>
+        <p>Estado: {{ $comunidad->estado_comunidad }}</p>
+    </div>
+</x-app-layout>
