a6fd8410019d1ad4eee04fc6e7a06c88ffb4c0c1
Daniel Garcia Figueroa <dangarciafigueroa19@gmail.com>
2024-03-18 22:09:00 -0600

    Modify button crear publicacion comunidad

--- resources/views/comunidades/show.blade.php
+++ resources/views/comunidades/show.blade.php
@@ -29,11 +29,13 @@
                 </svg>
                 <p class="text-sm">{{ $comunidad->num_participantes }}</p>
             </div>
-            <button class="bg-gray-600 text-white px-4 py-2 rounded-md"> + Crea una publicación</button>
+            <button class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" 
+                    onclick="Livewire.dispatch('openModal', { component: 'create-post', arguments: {comunidad_id:{{$comunidad->id}}, }})">Crear Post</button>
+                    @livewire('wire-elements-modal')
             <button class="bg-blue-500 text-white px-4 py-2 rounded-md">Unirse</button>
         </div>
     </div>
-    
+
     <details class="px-4 py-4 border border-gray-300 bg-gray-200 rounded-md">
         <summary class="cursor-pointer">Detalles</summary>
         <div class="mt-2">
