6a05d726fc1535f56d9884a008179cfa3e2399fd
Daniel Garcia Figueroa <dangarciafigueroa19@gmail.com>
2024-03-19 21:23:27 -0600

    Modify create view comunidades

--- resources/views/comunidades/create.blade.php
+++ resources/views/comunidades/create.blade.php
@@ -1,63 +1,72 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Create comunidad</title>
-</head>
-<body>
-    <h1>Crear comunidad</h1>
+<x-app-layout>
+    <button class="my-3 mx-20 p-1 bg-gray-400 rounded-lg">
+        <a href="{{ route('comunidades.index')}}">← Back to index</a>
+    </button>
+    <div class="my-2 mx-60 p-5 bg-gray-300 rounded-lg">
+        <h1 class="font-serif font-bold text-2xl text-center mb-4">Crear comunidad</h1>
 
-    <a href="{{ route('comunidades.index')}}">Back to index</a><br><br>
-
-    <form action="{{ route('comunidades.store') }}" method="POST">
-        @csrf
-        <label for="nombre">Nombre</label>
-        <input type="text" name="nombre" id="nombre"><br>
-        @error('nombre')
-            <p style="color: red">{{ $message }}</p>
-        @enderror
-
-        <label for="descripcion">Descripción</label>
-        <textarea name="descripcion" id="descripcion" cols="20" rows="5"></textarea><br>
-        @error('descripcion')
-            <p style="color: red">{{ $message }}</p>
-        @enderror
-
-        <div class="mt-4">
-                <x-label for="centro" value="{{__('Centro')}}" />
-                <select name="centro" id="centro">
-                    <option value="0">Seleccione un centro</option>
-                    @foreach ($centros as $centro)
-                        <option value="{{$centro->id}}">{{$centro->nombre}}</option>
-                    @endforeach
-                </select>
-        <div>
-
-        <label for="reglas">Reglas</label>
-        <textarea name="reglas" id="reglas" cols="20" rows="5"></textarea><br>
-        @error('reglas')
-            <p style="color: red">{{ $message }}</p>
-        @enderror
-
-        <label for="banner">Banner</label>
-        <input type="file" name="banner" id="banner"><br>
-        @error('banner')
-            <p style="color: red">{{ $message }}</p>
-        @enderror
-
-        <label for="estado_comunidad">Estado de la comunidad</label>
-        <select name="estado_comunidad" id="estatus">
-            <option value="activo">Activa</option>
-            <option value="inactivo">Inactivo</option>
-        </select><br>
-        @error('estado_comunidad')
-            <p style="color: red">{{ $message }}</p>
-        @enderror
-
-        <button type="submit">Crear</button>
-    </form>
+        <form action="{{ route('comunidades.store') }}" method="POST" class="space-y-3 mx-10">
+            @csrf
+            <div class="flex space-x-3">
+                <div class="w-1/2 flex flex-col">
+                    <label for="nombre" class="mb-1">Nombre</label>
+                    <input type="text" name="nombre" id="nombre" class="border rounded-md px-2 py-1 text-sm">
+                    @error('nombre')
+                        <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
     
-</body>
-</html>
+                <div class="w-1/2 flex flex-col">
+                    <x-label for="centro" :value="__('Centro')" class="mb-2" />
+                    <select name="centro" id="centro" class="border rounded-md px-2 py-1 text-sm">
+                        <option value="0">Seleccione un centro</option>
+                        @foreach ($centros as $centro)
+                            <option value="{{$centro->id}}">{{$centro->nombre}}</option>
+                        @endforeach
+                    </select>
+                </div>
+            </div>
+
+            <div class="flex flex-col">
+                <label for="descripcion" class="mb-1">Descripción</label>
+                <textarea name="descripcion" id="descripcion" cols="20" rows="5" class="border rounded-md px-2 py-1 text-sm resize-none"></textarea>
+                @error('descripcion')
+                    <p class="text-red-500">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="flex flex-col">
+                <label for="reglas" class="mb-1">Reglas</label>
+                <textarea name="reglas" id="reglas" cols="20" rows="5" class="border rounded-md px-2 py-1 text-sm resize-none"></textarea>
+                @error('reglas')
+                    <p class="text-red-500">{{ $message }}</p>
+                @enderror
+            </div>
+
+            <div class="flex space-x-3">
+                <div class="w-1/2 flex flex-col">
+                    <label for="banner" class="mb-1">Banner</label>
+                    <input type="file" name="banner" id="banner" class="border rounded-md px-2 py-1 text-sm">
+                    @error('banner')
+                        <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+    
+                <div class="w-1/2 flex flex-col">
+                    <label for="estado_comunidad" class="mb-1">Estado de la comunidad</label>
+                    <select name="estado_comunidad" id="estatus" class="border rounded-md px-2 py-1 text-sm">
+                        <option value="activo">Activa</option>
+                        <option value="inactivo">Inactivo</option>
+                    </select>
+                    @error('estado_comunidad')
+                        <p class="text-red-500">{{ $message }}</p>
+                    @enderror
+                </div>
+            </div>
+            
+            <div class="flex justify-end">
+                <button type="submit" class="bg-blue-800 text-white font-bold py-2 px-4 rounded">Crear</button>
+            </div>
+        </form>
+    </div>
+</x-app-layout>
--- resources/views/comunidades/show.blade.php
+++ resources/views/comunidades/show.blade.php
@@ -32,13 +32,13 @@
                 </svg>
                 <p class="text-sm">{{ $comunidad->usuarios()->count() }}</p>
             </div>
-            <button class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" 
+            <button class="inline-flex items-center px-4 py-2 bg-gray-600 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" 
                     onclick="Livewire.dispatch('openModal', { component: 'create-post', arguments: {comunidad_id:{{$comunidad->id}} }})">Crear Post
             </button>
                 @livewire('wire-elements-modal')
             <form action="{{ route('comunidad.follower', $comunidad->id) }}" method="post">
                 @csrf
-                <button class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                <button class="inline-flex items-center px-4 py-2 bg-blue-800 dark:bg-blue-500 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
                     Unirse
                 </button>
             </form>
