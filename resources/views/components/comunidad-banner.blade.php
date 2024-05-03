2e130eae592212e96dd69880709252d66ec69508
Daniel <dangarciafigueroa19@gmail.com>
2024-05-03 15:23:36 -0600

    Fix button crear post and event when user is not in the comunidad

--- resources/views/components/comunidad-banner.blade.php
+++ resources/views/components/comunidad-banner.blade.php
@@ -10,6 +10,12 @@
     $colors = ['bg-red-500', 'bg-blue-500', 'bg-green-500', 'bg-yellow-500', 'bg-indigo-500', 'bg-purple-500', 'bg-pink-500'];
     $banner_color = $colors[array_rand($colors)]; 
 @endphp
+@php
+    $usuario_unido = DB::table('comunidad_users')
+        ->where('comunidad_id', $comunidad->id)
+        ->where('user_id', Auth::id())
+        ->exists();
+@endphp
     <div class="my-2">
         @if($comunidad->banner)
             <img src="{{ asset('storage/' . $comunidad->banner->ubicacion) }}" alt="{{$comunidad->nombre}} banner" class="w-auto m-auto h-[150px] dark:text-white">
@@ -53,19 +59,15 @@
                 </svg>
                 <p class="text-sm">{{ $comunidad->usuarios()->count() }}</p>
             </div>
+            @if($usuario_unido)
             <button class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" 
                     onclick="Livewire.dispatch('openModal', { component: 'create-post', arguments: {comunidad_id:{{$comunidad->id}}}})">Crear Post
             </button>
             <button class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150"
                     onclick="Livewire.dispatch('openModal', { component: 'create-event', arguments: {comunidad_id:{{$comunidad->id}}}})">Crear Evento
             </button>
+            @endif
                 @livewire('wire-elements-modal')
-                @php
-                    $usuario_unido = DB::table('comunidad_users')
-                                        ->where('comunidad_id', $comunidad->id)
-                                        ->where('user_id', Auth::id())
-                                        ->exists();
-                @endphp
             @if(!$usuario_unido)
             <form action="{{ route('comunidad.follower', $comunidad->id) }}" method="post">
                 @csrf
